0c41040c16aed18c8c3c8423dd79522a1c29f6cc
KrzysztofPio <krzysztofp10git@op.pl>
2021-04-18 14:01:53 +0200

    ready for presentation on classes

--- Studia/Sem4/OptymalizacjaKombinatoryczna/GraphColoring/src/main/java/GraphColoring.java
+++ Studia/Sem4/OptymalizacjaKombinatoryczna/GraphColoring/src/main/java/GraphColoring.java
@@ -1,14 +1,14 @@
 import java.io.IOException;
 
 /**
- * Implementation of greedy algorithm for colouring graph. Fast run 8h. To obtain right results:
+ * Implementation of greedy algorithm for colouring graph. Fast run 6h. To obtain right results:
  * - the generated graph must be saved to a file
  * - load graph from file
  * - use graph as argument for AlgorithmGreedy constructor
  */
 public class GraphColoring {
     public static void main(String[] args) throws IOException {
-
+/*
         //gen graph
         Generator genGraph = new Generator(100, 50);
         //create file name
@@ -17,12 +17,16 @@ public class GraphColoring {
         newFileName.append("size"+String.valueOf(genGraph.getSize())+"_sat"+String.valueOf(genGraph.getSaturation()));
         newFileName.append(".txt");
         //save file
-        ReadWrite rw = new ReadWrite(newFileName.toString());
-
+        ReadWrite generatedRW = new ReadWrite(newFileName.toString());
+        generatedRW.write(genGraph.toString());
+        //-----------------------------------------------------------------------------
+*/
         //read file
-        rw.write(genGraph.toString());
+        ReadWrite fileRW = new ReadWrite("queen6.txt");
+
         //solve graph
-        Graph graph = new Graph(rw.read());
+        Graph graph = new Graph(fileRW.read());
+        System.out.println(graph);
         AlgorithmGreedy greedy = new AlgorithmGreedy(graph);
         System.out.println("Uzyto " + (greedy.getHighestUsedColor()+1) + " kolorów:");
         System.out.println(greedy.toStringColoredVertexes());
